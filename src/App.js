825fbb6f5d8769bd6eca7ada5f88785865d3cd8f
johnnybravo916 <johnong916@gmail.com>
2020-06-29 08:00:54 -0500

    added data from wordpress api, added projects page, added react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Switch, Route } from "react-router-dom";
 
 import Grid from "./components/grid.component";
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-const ProjectsPage  = () => {
-  return(
-    <div>
-      Projects
-    </div>
-  )
-}
+const ProjectsPage = () => {
+    const [projects, setProjects] = useState([]);
 
-export default ProjectsPage ;
+    useEffect(() => {
+        let projectsUrl = "http://yaokana.com/wp-json/wp/v2/projects?embed";
+        fetch(projectsUrl)
+            .then((projectData) => projectData.json())
+            .then((projectData) => setProjects(projectData));
+    }, []);
+
+    return (
+        <main className="linkgrid" style={{backgroundColor:'#000'}}>
+            <div className="linkgrid__wrapper">
+                {projects.map((project, index) => {
+                    return (
+                    <div key={index} className="linkgrid__linkwrap">
+                      <a href={project.link} title={project.title.rendered}>{project.title.rendered}</a>
+                      </div>
+                    )
+                    ;
+                })}
+            </div>
+        </main>
+    );
+};
+
+export default ProjectsPage;
