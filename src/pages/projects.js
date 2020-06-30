4e4c234882cdc3d815f2c7096995b2810623f31b
johnnybravo916 <johnong916@gmail.com>
2020-06-30 09:35:24 -0500

    added background to projects page but it's too slow

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,25 +1,44 @@
 import React, { useEffect, useState } from "react";
 
-const ProjectsPage = () => {
+const ProjectsPage = (props) => {
     const [projects, setProjects] = useState([]);
 
     useEffect(() => {
-        let projectsUrl = "http://yaokana.com/wp-json/wp/v2/projects?embed";
+        let projectsUrl = "http://yaokana.com/wp-json/wp/v2/projects?_embed";
         fetch(projectsUrl)
             .then((projectData) => projectData.json())
-            .then((projectData) => setProjects(projectData));
+            .then((projectData) => setProjects([...projectData]));
     }, []);
 
+    const getBackground = (background) => {
+        const linkgrid = document.querySelector('.linkgrid');
+        linkgrid.style.backgroundImage = `url(${background})`;
+        // too slow, will think of something else, maybe show all the images then hide/show on hover
+    };
+
     return (
-        <main className="linkgrid" style={{backgroundColor:'#000'}}>
+        <main className="linkgrid" style={{ backgroundImage: "url('http://yaokana.com/wp/wp-content/uploads/2018/08/yaokana_watermark_FeaturedImage-1140x600.jpg')" }}>
             <div className="linkgrid__wrapper">
                 {projects.map((project, index) => {
                     return (
-                    <div key={index} className="linkgrid__linkwrap">
-                      <a href={project.link} title={project.title.rendered}>{project.title.rendered}</a>
-                      </div>
-                    )
-                    ;
+                        <div
+                            key={index}
+                            data-img={
+                                project._embedded["wp:featuredmedia"][0]
+                                    .media_details.sizes.large.source_url
+                            }
+                            className="linkgrid__linkwrap"
+                            onMouseOver={() => getBackground(project._embedded["wp:featuredmedia"][0]
+                            .media_details.sizes.large.source_url)}
+                        >
+                            <a
+                                href={project.link}
+                                title={project.title.rendered}
+                            >
+                                {project.title.rendered}
+                            </a>
+                        </div>
+                    );
                 })}
             </div>
         </main>
