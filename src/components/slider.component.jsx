b2148d6bb3d17a59b98596c5545ea6ae9959ec38
johnnybravo916 <johnong916@gmail.com>
2020-07-07 07:39:33 -0500

    added link below featured project name

--- src/components/slider.component.jsx
+++ src/components/slider.component.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import AutoplaySlider from "react-awesome-slider";
-import "react-awesome-slider/dist/styles.css";
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
 
 import axios from "axios";
 import parse from "html-react-parser";
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 const Slider = () => {
     const [content, setContent] = useState([]);
@@ -16,8 +19,8 @@ const Slider = () => {
             .get(contentUrl)
             .then((contentData) => {
                 console.log(contentData.data);
-                const featuredCategories = contentData.data.filter(content => 
-                  content.categories.every(category => category === 20)
+                const featuredCategories = contentData.data.filter((content) =>
+                    content.categories.every((category) => category === 20)
                 );
                 setContent([...featuredCategories]);
             })
@@ -27,31 +30,34 @@ const Slider = () => {
     }, []);
 
     return (
-        <AutoplaySlider play={true} bullets={false}>
+        <AutoplaySlider
+            play={true}
+            bullets={false}
+            cancelOnInteraction={false}
+            interval={2000}
+        >
             {content.map((content, index) => {
-              return (
-                <div className="slider__element" key={index} style={{
-                  backgroundImage: `url(${content._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url})`,
-              }}>
-                  <h2>{parse(content.title.rendered)}</h2>
-                  <span className="slider__element--number">0{index + 1}</span>
-                </div>
-              )
+                return (
+                    <div
+                        className="slider__element"
+                        key={index}
+                        style={{
+                            backgroundImage: `url(${content._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url})`,
+                        }}
+                    >
+                        <Link to={`/${content.slug}`} className="slider__title">
+                            <h2>{parse(content.title.rendered)}</h2>
+                        </Link>
+                        <Link to={`/${content.slug}`} className="slider__link">
+                            click for more
+                        </Link>
+                        <span className="slider__element--number">
+                            0{index + 1}
+                        </span>
+                    </div>
+                );
             })}
         </AutoplaySlider>
     );
 };
 export default Slider;
-
-
-{/* <AutoplaySlider play={true} bullets={false}>
-{content.map((content, index) => {
-  return (
-    <React.Fragment key={index}>
-    {content.categories.map((category, index) => {
-      return (<div key={index}>{category}</div>)
-    })}
-    </React.Fragment>
-  )
-})}
-</AutoplaySlider> */}
